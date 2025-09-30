059443b612076984a1e9afbfba38feb1acb54432
BOI0808 <nhatkhoi30@gmail.com>
2025-09-30 10:55:57 +0700

    update

---
+++ assets/main.js
@@ -0,0 +1,65 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const songs = [
+  {
+    name: "Nevada",
+    singer: "Vicetone",
+    path: "./assets/music/Nevada.mp3",
+    image: "./assets/img/Nevada.jpg",
+  },
+  {
+    name: "Summer Time",
+    singer: "K-391",
+    path: "./assets/music/Summertime.mp3",
+    image: "./assets/img/Summertime.jpg",
+  },
+  {
+    name: "Monody",
+    singer: "TheFatRat",
+    path: "./assets/music/Monody.mp3",
+    image: "./assets/img/Monody.jpg",
+  },
+  {
+    name: "Reality",
+    singer: "Lost Frequencies",
+    path: "./assets/music/Reality.mp3",
+    image: "./assets/img/Nevada.jpg",
+  },
+  {
+    name: "Waiting For Love",
+    singer: "Avicii",
+    path: "./assets/music/Waitingforlove.mp3",
+    image: "./assets/img/Waitingforlove.jpg",
+  },
+  {
+    name: "Lemon Tree",
+    singer: "DJ DESA REMIX",
+    path: "./assets/music/Lemontree.mp3",
+    image: "./assets/img/Lemontree.jpg",
+  },
+  {
+    name: "Sugar",
+    singer: "Maroon 5",
+    path: "./assets/music/Sugar.mp3",
+    image: "./assets/img/Sugar.jpg",
+  },
+  {
+    name: "My love",
+    singer: "Westlife",
+    path: "./assets/music/Mylove.mp3",
+    image: "./assets/img/Mylove.jpg",
+  },
+  {
+    name: "Attention",
+    singer: "Charlie Puth",
+    path: "./assets/music/Attention.mp3",
+    image: "./assets/img/Attention.jpg",
+  },
+  {
+    name: "Monster",
+    singer: "Katie sky",
+    path: "./assets/music/Monster.mp3",
+    image: "./assets/img/Monster.jpg",
+  },
+];
